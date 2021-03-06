78e266611bd832bdb8d787caea2265b70441f5a3
Rakesh <menariarakesh@gmail.com>
2021-03-06 23:27:14 +0530

    Transformers development: Initial commit

---
+++ src/main/java/com/aequilibrium/test/transformers/entity/Transformer.java
@@ -0,0 +1,10 @@
+package com.aequilibrium.test.transformers.entity;
+
+import lombok.Data;
+
+@Data
+public class Transformer {
+    private String id;
+    private String type;
+    // TODO(rakesh): add more fields
+}
